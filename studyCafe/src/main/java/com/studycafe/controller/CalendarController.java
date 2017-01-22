7e43003916382a526ba4dffcc8e173f380380abf
gaeun917 <stila917@gmail.com>
2017-01-22 14:08:33 +0900

    code review and indentation /comments on CalendarController, HomeController

--- studyCafe/src/main/java/com/studycafe/controller/CalendarController.java
+++ studyCafe/src/main/java/com/studycafe/controller/CalendarController.java
@@ -1,57 +1,37 @@
 package com.studycafe.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-import javax.swing.plaf.synth.SynthSeparatorUI;
-
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Controller;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.studycafe.model.dao.CalendarDao;
-import com.studycafe.model.dao.MemberDao;
 import com.studycafe.model.dto.Calendar;
 import com.studycafe.model.dto.Member;
 import com.studycafe.model.dto.Page;
 import com.studycafe.model.dto.PageMenu;
 import com.studycafe.model.service.CalendarService;
 import com.studycafe.model.service.PageService;
-import com.studycafe.model.service.UploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/calendar")
 public class CalendarController{
 
-	@InitBinder
+	@InitBinder // Date Form과 관련 있어 보임...
 	protected void initBinder(WebDataBinder binder) {
 	    SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
 	    binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
 	}
-
-	/*@Autowired
-	@Qualifier(value = "oracleCalendarDao")
-	private CalendarDao dao;
-	public void setCalendarDao(CalendarDao calendarDao) {
-		this.dao = calendarDao;
-	}*/
 	
 	@Autowired
 	@Qualifier("pageService")
@@ -61,11 +41,12 @@ public class CalendarController{
 	@Qualifier("calendarService")
 	private CalendarService calendarService;
 		
-	//2. 일정 달력에 전부 보여주기
+	//1. 일정 달력에 전부 보여주기
 	@RequestMapping(value = "list.action", method = RequestMethod.GET)
 	public ModelAndView list(Model model, HttpSession session, int memberpageno) {
-		
 		ModelAndView mav = new ModelAndView();
+
+		//1. loginuser로부터 pageNo, pageMenu, pageMenuByPageNotice, CalendarList 구하기
 		Member member = (Member) session.getAttribute("loginuser");
 
 		List<Page> pages = pageService.searchPageNoByMemberNo(member.getMemberNo());
@@ -74,120 +55,115 @@ public class CalendarController{
 		PageMenu noticeMenu = pageService.selectPageMenuByPageNoNotice(memberpageno);
 		
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		
+
+		//2. Calendar 정보 구하기- JSON형식으로
 		String json = "[";
-		for (int i = 0; i < calendars.size(); i++) {
-				Calendar c = calendars.get(i);
-				System.out.println(c.getContent());
-				System.out.println(c.getTitle());
+			for(Calendar c:calendars) {
+				//c = calendars.get(i);
 				json += "{";
-				json += "title : '" + c.getTitle() + "',";
-				json += "url : 'localhost:8088/studyCafe/calendar/view.action?calendarno=" + c.getCalendarNo() + "&memberpageno=" + memberpageno + "&noticemenu=" + noticeMenu.getMenuNo() +"',";
-				json += "start : '" + format.format(c.getStartDate()) + "'";
-				if(c.getDueDate()!=null){//마감일 없을 시
-					json += ", end : '" + format.format(c.getDueDate()) + "'";
-				}
+					json += "title : '" + c.getTitle() + "',";
+					json += "url : 'localhost:8088/studyCafe/calendar/view.action?" +
+							"calendarno=" + c.getCalendarNo() +
+							"&memberpageno=" + memberpageno +
+							"&noticemenu=" + noticeMenu.getMenuNo() +"',";
+					json += "start : '" + format.format(c.getStartDate()) + "'"; // 시작일
+
+					if(c.getDueDate()!=null){
+					json += ", end : '" + format.format(c.getDueDate()) + "'"; // 마감일(마감일 없으면 정보 보내지 않음)
+					}
 				json += "}";
-								
-				if (i < calendars.size() - 1) {
-					json += ",";
-				}
-		}	
-		json += "]";		
-		
-		mav.setViewName("calendar/list");
+			}
+		json += "]"; // JSON END
+
+		//3. mav에 add&set
 		mav.addObject("pagemenus", pageMenu);
 		mav.addObject("pages", pages);
 		mav.addObject("calendars", calendars);
 		mav.addObject("calendarJson", json);
 		mav.addObject("memberpageno", memberpageno);
 		mav.addObject("noticemenu", noticeMenu);
-		
+
+		mav.setViewName("calendar/list");
 		return mav;
-//		return "calendar/list";
-	}	
-	//0. 일정 작성 폼
+	}
+
+
+	//2. 일정 작성 폼
 	@RequestMapping(value = "writeform.action", method = RequestMethod.GET)
-	public ModelAndView getScheduleWriteForm(HttpSession session, int memberpageno) {
-		
+	public ModelAndView getScheduleWriteForm(int memberpageno) {
 		ModelAndView mav = new ModelAndView();
-		
+
+		// 1. pageMenu, pageMenuByPageNotice 구하기
 		List<PageMenu> pageMenu = pageService.selectPageMenuByPageNo(memberpageno);
 		PageMenu noticeMenu = pageService.selectPageMenuByPageNoNotice(memberpageno);
-		
-		mav.setViewName("calendar/writeform");
+
+		//2. mav add&set
 		mav.addObject("pagemenus", pageMenu);
 		mav.addObject("memberpageno", memberpageno);
 		mav.addObject("noticemenu", noticeMenu);
-		
+
+		mav.setViewName("calendar/writeform");
 		return mav;
-//		return "calendar/writeform";
 	}
-	
-	//1. 일정 등록하기
+
+
+	//3. 일정 등록하기
 	@RequestMapping(value = "write.action", method = RequestMethod.POST)
-	public ModelAndView calendar(Calendar calendar, HttpSession session, int memberpageno) {
-	
+	public ModelAndView calendar(Calendar calendar, int memberpageno) {
 		ModelAndView mav = new ModelAndView();
-		
-		
+
+		// 1. pageMenu, pageMenuByPageNotice구하기
 		List<PageMenu> pageMenu = pageService.selectPageMenuByPageNo(memberpageno);
 		PageMenu noticeMenu = pageService.selectPageMenuByPageNoNotice(memberpageno);
+
+		//2. calendar 등록
 		calendar.setPageNo(memberpageno);
 		calendarService.registerCalendar(calendar);
-		
-		mav.setViewName("redirect:/calendar/list.action");
+
+		//3. mav add&set
 		mav.addObject("pagemenus", pageMenu);
 		mav.addObject("memberpageno", memberpageno);
 		mav.addObject("noticemenu", noticeMenu);
-		
+
+		mav.setViewName("redirect:/calendar/list.action");
 		return mav;
-//		return "redirect:/calendar/list.action";
-	} 
-	
-	//3.일정 클릭 시 상세 보여주기
+	}
+
+
+	//4.일정 클릭 시 상세 보여주기
 	@RequestMapping(value = "view.action", method = RequestMethod.GET)
-	public ModelAndView view(@RequestParam("calendarno") int calendarNo, Model model, HttpSession session,
-			int memberpageno) {
-		
+	public ModelAndView view(@RequestParam("calendarno")int calendarNo, int memberpageno) {
 		ModelAndView mav = new ModelAndView();
-		
+
+		//1. pageMenu, noticeMenu 구하기
 		List<PageMenu> pageMenu = pageService.selectPageMenuByPageNo(memberpageno);
 		PageMenu noticeMenu = pageService.selectPageMenuByPageNoNotice(memberpageno);
-		
+
+		//2. Calendar, Calendarno 구하기
 		Calendar calendar = calendarService.searchCalendarByCalendarNo(calendarNo);
-//		model.addAttribute("calendar", calendar);
-		
-		int c = calendar.getCalendarNo();
-//		model.addAttribute("c", c);
-		
+		int calendarno = calendar.getCalendarNo();
+
+		//2. Calendar start & due date 구하기
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		String startDate2 = format.format(calendar.getStartDate());
 		String dueDate2 = format.format(calendar.getDueDate());
-		
-		System.out.println(startDate2);
-		System.out.println(dueDate2);
-		
-		mav.setViewName("calendar/view");
+
+		//3.mav add & set
 		mav.addObject("pagemenus", pageMenu);
-		mav.addObject("calendar", calendar);
-		
-		mav.addObject("c", c);
-		mav.addObject("memberpageno", memberpageno);
 		mav.addObject("noticemenu", noticeMenu);
+		mav.addObject("calendar", calendar);
+		mav.addObject("calendarno", calendarno);
+		mav.addObject("memberpageno", memberpageno);
 		mav.addObject("startDate2", startDate2);
 		mav.addObject("dueDate2", dueDate2);
-		
+
+		mav.setViewName("calendar/view");
 		return mav;
-//		return "calendar/view";
 	}
+
+	//5. 삭제
 	@RequestMapping(value = "delete.action", method = RequestMethod.GET)
 	public String delete(@RequestParam("calendarno") int calendarNo, int memberpageno) {
-		//1. 요청 데이터 읽기 (글번호)
-//		String uploadNo = req.getParameter("uploadno");
-//		if (uploadNo == null || uploadNo.length() == 0) {
-//			return "redirect:/upload/list.action";
-//		}
 
 		//2. 데이터 처리 (db에서 데이터 변경)
 		calendarService.deleteCalendar(calendarNo);
--- studyCafe/src/main/java/com/studycafe/controller/HomeController.java
+++ studyCafe/src/main/java/com/studycafe/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.studycafe.controller;
 
-import java.util.List;
-import java.util.Locale;
-
+import com.studycafe.model.dto.Board;
+import com.studycafe.model.dto.BoardMember;
+import com.studycafe.model.dto.Page;
+import com.studycafe.model.service.BoardService;
+import com.studycafe.model.service.MemberService;
+import com.studycafe.model.service.PageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -10,34 +13,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.studycafe.model.dto.Board;
-import com.studycafe.model.dto.BoardMember;
-import com.studycafe.model.dto.Member;
-import com.studycafe.model.dto.Page;
-import com.studycafe.model.mapper.MemberMapper;
-import com.studycafe.model.service.BoardService;
-import com.studycafe.model.service.CalendarService;
-import com.studycafe.model.service.MemberService;
-import com.studycafe.model.service.PageBoardService;
-import com.studycafe.model.service.PageService;
-import com.studycafe.model.service.UploadService;
+import java.util.List;
+import java.util.Locale;
 
 @Controller
 public class HomeController {
-	
-//	@RequestMapping(value = "/", method = RequestMethod.GET)
-//	public String home1(Locale locale, Model model) {		
-//		return "index"; // /WEB-INF/views/ + index + .jsp
-//	}	
-//	@RequestMapping(value = "/home.action", method = RequestMethod.GET)
-//	public String home2(Locale locale, Model model) {		
-//		return "index"; // /WEB-INF/views/ + index + .jsp
-//	}	
+
 	//2개 이상의 경로를 배열로 묶어서 매핑 가능
-	
-	@Autowired
-	@Qualifier("pageBoardService")
-	private PageBoardService pageBoardService;
+	//	@RequestMapping(value = "/", method = RequestMethod.GET)
+	//	public String home1(Locale locale, Model model) {
+	//		return "index"; // /WEB-INF/views/ + index + .jsp
+	//	}
+
+	//	@RequestMapping(value = "/home.action", method = RequestMethod.GET)
+	//	public String home2(Locale locale, Model model) {
+	//		return "index"; // /WEB-INF/views/ + index + .jsp
+	//	}
+
 
 	@Autowired
 	@Qualifier("pageService")
@@ -46,52 +38,43 @@ public class HomeController {
 	@Autowired
 	@Qualifier("memberService")
 	private MemberService memberService;
-	
-	@Autowired
-	@Qualifier("calendarService")
-	private CalendarService calendarService;
-	
-	@Autowired
-	@Qualifier("uploadService")
-	private UploadService uploadService;
-	
+
 	@Autowired
 	@Qualifier("boardService")
 	private BoardService boardService;
 	
 	@RequestMapping(value = { "/", "home.action" }, method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		// board 테이블의 closedate와 sysdate의 차가 음수일 경우  board테이블의 status를 0으로 설정
-		List<Board> boards = boardService.selectBoardListByTime();
-		if(boards != null){
-			for(Board board : boards){
-				boardService.deleteBoard(board.getBoardNo());
-				System.out.println(board.getCloseDate());
-				
+
+		//1. boardList 가져와서 삭제
+		List<Board> boards = boardService.selectBoardListByTime();  // 등록된 전체 리스트를 다 가져 오나 봄
+		if(boards != null){											// 1.1 board 리스트가 있으면
+			for(Board board : boards){								// Board item:BList 다 돌려서
+				boardService.deleteBoard(board.getBoardNo());		// 전체 삭제
+
+		//2. page 마감일 등록
 				Page page = new Page();
-				page.setCloseDate(board.getCloseDate());
-				
-				pageService.insertPage(page);
+				page.setCloseDate(board.getCloseDate());			// page 마감일 설정
+				pageService.insertPage(page);						// page db에 등록
+
+		//3. 모임 #로 모임멤버 찾기
+				List<BoardMember> members =
+						memberService.selectBoardMemberByBoardNo(board.getBoardNo());
 				int pageNo = pageService.selectPageNo();
-				
-				List<BoardMember> members = memberService.selectBoardMemberByBoardNo(board.getBoardNo());
-				if(members != null){
-					for(BoardMember member : members){
-						
-						pageService.insertMemberPageByMemberNo(member.getMemberNo(), pageNo, board.getTitle());
+				if(members != null){								// 멤버가 있으면
+					for(BoardMember member : members){				// DTO에 get해서 insert함
+						pageService.insertMemberPageByMemberNo( member.getMemberNo(), pageNo, board.getTitle());
 					}
-					
 				}
 				pageService.insertPageMenuNotice(pageNo);
 			}
-			
 		}
-//		// board 테이블의 status가 0인 board테이블의 board_no로 member_page의 같은 board_no가 존재하는지 확인
-//		int memberPageCount = pageService.selectMemberPageCountByMemberNo(memberNo);
+		// closedate - sysdate < 0 : board DTO status =0으로 설정
+		// (board dto) status = 0인 board_no == (member_page) board_no가 존재하는지 확인
+		// int memberPageCount = pageService.selectMemberPageCountByMemberNo(memberNo);
+
 		// page_menu(공지사항, status 5), page_calendar(행하나 추가)
-		
-		// page_upload테이블의 새로운 행을 생성 () 
-		
+		// page_upload테이블의 새로운 행을 생성 ()
 		// member_board 테이블에 등록시킴(board_no, member_no) <<참가 인원!
 		
 		return "index"; // /WEB-INF/views/ + index + .jsp
